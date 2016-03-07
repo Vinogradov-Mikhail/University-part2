3e47135211d15a851055c2f82b2da3907b9f6a4b
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2016-03-07 21:13:51 +0300

    2_3

---
+++ 2_3/2_3/List.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class List
+    {
+        /// <summary>
+        /// class for element of stack
+        /// </summary>
+        class Element
+        {
+            public string Value { get; set; }
+            public Element Next { get; set; }
+
+            /// <summary>
+            /// constructor for stack element
+            /// </summary>
+            /// <param name="_value"></param>
+            /// <param name="next"></param>
+            public Element(string _value = "", Element next = null)
+            {
+                this.Value = _value;
+                this.Next = next;
+            }
+        }
+        private Element head = null;// new Element();
+        private int _count;
+
+        /// <summary>
+        /// stack constructor 
+        /// </summary>
+        public List()
+        {
+            _count = -1;
+        }
+
+        /// <summary>
+        /// add element in stack
+        /// </summary>
+        /// <param name="value"></param> 
+        public void Push(string value)
+        {
+            head = new Element(value, head);
+            ++_count;
+        }
+
+        /// <summary>
+        /// delete element from list
+        /// </summary>
+        /// <returns></returns>
+        public string Pop(string elem)
+        {
+            if (head == null)
+                return "";
+            if (head.Value == elem)
+            {
+                string tmp = head.Value;
+                head = head.Next;
+                --_count;
+                return tmp;
+            }
+            Element temp = head;
+            Element secondTemp = head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+                if (temp.Value == elem)
+                {
+                    secondTemp.Next = temp.Next;
+                    --_count;
+                    return temp.Value;
+                }
+                secondTemp = temp;
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// print all stack
+        /// </summary>
+        public void Print()
+        {
+            Element tmp = head;
+            do
+            {
+                Console.Write(tmp.Value + " ");
+                tmp = tmp.Next;
+            } while (tmp != null);
+        }
+
+        public bool HaveElements()
+        {
+            return _count != -1;
+        }
+
+        /// <summary>
+        /// find element in list
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public bool Find(string element)
+        {
+            Element tmp = head;
+            for (int i = 0; i <= _count; ++i)
+            {
+                if (tmp.Value == element)
+                {
+                    return true;
+                }
+                tmp = tmp.Next;
+            }
+            return false;
+        }
+    }
+}
---
+++ 2_3/2_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hash tmp = new Hash();
+            tmp.Add("trep");
+            tmp.Add("pert");
+            tmp.Add("trop");
+            tmp.Add("treerered");
+            tmp.Add("trep");
+            tmp.Add("123");
+            tmp.Search("pert");
+            tmp.Print();
+            tmp.Delete("123");
+            tmp.Print();
+        }
+    }
+}
