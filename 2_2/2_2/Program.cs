058e9814ec8a4a657d6bbee3f80ba0fd599aae51
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2016-03-08 23:17:38 +0300

    2__2

---
+++ 2_2/2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List tmp = new List();
+            for (int i = 0; i < 10; ++i)
+            {
+                tmp.Push(i);
+            }
+            tmp.InIndex(1, 11);
+            tmp.RemomeAt(3);
+            tmp.Pop(11);
+            tmp.Print();
+        }
+    }
+}
