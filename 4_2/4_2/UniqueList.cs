105aa396dd53ba1f0d7c0feb8ab0fbb9e86f76f9
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2016-03-13 23:58:30 +0300

    add 4_2

---
+++ 4_2/4_2/UniqueList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueListNamespace
+{
+    /// <summary>
+    /// class for list which have unique elements
+    /// </summary>
+    public class UniqueList : List
+    {
+        /// <summary>
+        /// add element in UniqueList. Throw exception if list have this value
+        /// </summary>
+        /// <param name="number"></param>
+        public void Add(int number)
+        {
+            if (Find(number))
+            {
+                throw new ULAddAvailableElement("Try add another element, because this element available in list");
+            }
+            else Push(number);
+        }
+
+        /// <summary>
+        /// delete element from UniqueList. Throw exception if list doesn't have this value
+        /// </summary>
+        /// <param name="number"></param>
+        public void Delete(int number)
+        {
+            if (Find(number))
+            {
+                Pop(number);
+            }
+            else throw new ULDeleteNotAvailableElement("Try delete another element, because this element not available in list");
+        }
+    }
+}
---
+++ 4_2/4_2test/UniquListTets.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniqueListNamespace;
+
+namespace UniqueListNamespace.Test
+{
+    [TestClass]
+    public class UniqueListTest
+    {
+        private UniqueList list;
+
+        [TestInitialize]
+        public void Inizialize()
+        {
+            list = new UniqueList();
+        }
+
+        [TestMethod]
+        public void AddAndDeleteTest()
+        {
+            list.Add(1);
+            list.Add(2);
+            Assert.AreEqual(1, list.Pop(1));
+            list.Delete(2);
+            list.Add(12);
+            Assert.AreEqual(0, list.Pop(2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ULAddAvailableElement))]
+        public void AddExceptionTest()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Delete(2);
+            list.Add(12);
+            list.Add(12);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ULDeleteNotAvailableElement))]
+        public void DeleteExceptionTest()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Delete(2);
+            list.Add(12);
+            list.Add(112);
+            list.Delete(11);
+        }
+    }
+}
