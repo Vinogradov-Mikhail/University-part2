d1665d54728dd8882f8559782f597855128bfb4b
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2016-03-17 22:31:06 +0300

    add homework 5 №1,2,3

---
+++ 5_123/UnitTestFunction/SomeFuncTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _5_123.Test
+{
+    [TestClass]
+    public class SomeFunctionTest
+    {
+        private SomeFunction func;
+        private List list;
+
+        [TestInitialize]
+        public void Inizialize()
+        {
+            func = new SomeFunction();
+            list = new List();
+        }
+
+        [TestMethod]
+        public void TestMap()
+        {
+            list.Push(1);
+            list.Push(2);
+            list.Push(3);
+            list = func.Map(list, x => x * 2);
+            Assert.AreEqual(6, list.Pop(6));
+            Assert.AreEqual(4, list.Pop(4));
+            Assert.AreEqual(2, list.Pop(2));
+        }
+
+        [TestMethod]
+        public void TestFilter()
+        {
+            list.Push(1);
+            list.Push(4);
+            list.Push(6);
+            list = func.Filter(list, x => x > 4);
+            Assert.AreEqual(6, list.Pop(6));
+            Assert.AreEqual(0, list.Pop(4));
+        }
+
+        [TestMethod]
+        public void TestFolder()
+        {
+            list.Push(1);
+            list.Push(2);
+            list.Push(3);
+            Assert.AreEqual(6, func.Fold(list, 1, (acc, elem) => acc * elem));
+        }
+    }
+}
